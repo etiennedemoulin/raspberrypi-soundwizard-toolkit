dd884cde587c73041e827a5981f7c9c12fa75998
etienne <etioud@gmail.com>
2023-11-21 22:19:33 +0100

    ok

--- vnc-on-startup/install.sh
+++ vnc-on-startup/install.sh
@@ -2,10 +2,10 @@
 
 sudo apt update
 sudo apt install -y tightvncserver
-sudo cp tightvncserver.service /etc/systemd/system/vncserver.service
-sudo chmod 644 /etc/systemd/system/vncserver.service
-sudo systemctl start vncserver
-sudo systemctl enable vncserver
+sudo cp tightvncserver.service /etc/systemd/system/vncserver@.service
+sudo chmod 644 /etc/systemd/system/vncserver@.service
+sudo systemctl start vncserver@1.service
+sudo systemctl enable vncserver@1.service
 # confirm "Would you like to reboot now ?" && sudo reboot
 
 echo "Connect via macos with cmd+k, vnc://raspberrypi.local:5901"
